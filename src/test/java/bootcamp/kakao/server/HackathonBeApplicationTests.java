0a01247e7acddc1c3a16b1f517af30a89caf1bad
eedo_y <doup2001@naver.com>
2025-12-17 22:16:22 +0900

    🔥fix : 테스트 프로파일 수정

--- src/test/java/bootcamp/kakao/server/HackathonBeApplicationTests.java
+++ src/test/java/bootcamp/kakao/server/HackathonBeApplicationTests.java
@@ -2,8 +2,10 @@ package bootcamp.kakao.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class HackathonBeApplicationTests {
 
     @Test
