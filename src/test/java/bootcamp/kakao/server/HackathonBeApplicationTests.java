1ac0848545989901eb8a613230d92ceb5de8d6d1
eedo_y <doup2001@naver.com>
2025-12-17 22:20:28 +0900

    🔥fix : 테스트 프로파일 설정 에러 수정 - Profile에서 ActiveProfiles

--- src/test/java/bootcamp/kakao/server/HackathonBeApplicationTests.java
+++ src/test/java/bootcamp/kakao/server/HackathonBeApplicationTests.java
@@ -3,9 +3,10 @@ package bootcamp.kakao.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class HackathonBeApplicationTests {
 
     @Test
