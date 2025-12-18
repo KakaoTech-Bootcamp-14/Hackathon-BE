1c91d6b0b308486e431b106f77ae523d5496ceaf
eedo_y <doup2001@naver.com>
2025-12-18 15:35:10 +0900

    📖docs: 스웨거 변수 추가

--- src/main/java/bootcamp/kakao/server/dto/chat/ChatSendRequest.java
+++ src/main/java/bootcamp/kakao/server/dto/chat/ChatSendRequest.java
@@ -1,10 +1,13 @@
 package bootcamp.kakao.server.dto.chat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ChatSendRequest {
+
+    @Schema(description = "채팅 메시지 내용", example = "Chapter 1의 핵심 개념을 설명해주세요.")
     private String content;
 }
